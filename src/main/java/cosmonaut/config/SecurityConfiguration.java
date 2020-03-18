4f3d68887c3c94e5d5bf2c792d2fa663a765f593
RenatFaiz <renatf87@gmail.com>
2020-03-18 18:39:25 +0400

    Redirection after logging created (for admin and user)

--- src/main/java/cosmonaut/config/SecurityConfiguration.java
+++ src/main/java/cosmonaut/config/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package cosmonaut.config;
 
+import cosmonaut.utils.DefaultAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.sql.DataSource;
 
@@ -25,17 +29,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //        auth.jdbcAuthentication().dataSource(dataSource);
 //    }
 
+    @Bean("authenticationManager")
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}pa").roles("USER")
-                .and().withUser("admin").password("{noop}pa").roles("ADMIN", "USER");
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}pa").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}pa").roles("ADMIN", "USER");
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new DefaultAuthenticationSuccessHandler();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
-                .and().formLogin();
+                    .antMatchers("/login*").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .successHandler(authenticationSuccessHandler());
+
 //                .loginPage("/login").permitAll()
 //                .loginProcessingUrl("/authenticateTheUser");
 
