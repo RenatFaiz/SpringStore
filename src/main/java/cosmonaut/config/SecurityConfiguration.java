6c1a21e120dc477eedffc5c27df9e209a51e9244
RenatFaiz <renatf87@gmail.com>
2020-03-27 13:23:04 +0400

    Schema name to remote DB added

--- src/main/java/cosmonaut/config/SecurityConfiguration.java
+++ src/main/java/cosmonaut/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package cosmonaut.config;
 
 import cosmonaut.util.DefaultAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
