bb24dc3572820d0f5aadaaedf1f7959f47c4041c
RenatFaiz <renatf87@gmail.com>
2020-04-17 16:36:31 +0400

    Add to cart added to Details template

--- src/main/java/cosmonaut/controller/CartController.java
+++ src/main/java/cosmonaut/controller/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -27,8 +28,13 @@ public class CartController {
     }
 
     @GetMapping("/add/{id}")
-    public String addToCart(@PathVariable("id") Long id) {
+    public String addToCart(@PathVariable("id") Long id,
+                            @RequestParam(value = "topage", defaultValue = "shop",
+                                    required = false) String query) {
         cart.addProductById(id);
-        return "redirect:/shop";
+        if (query.equals("stayonpage")) {
+            return "redirect:/details/{id}";
+        }
+        return "redirect:/" + query;
     }
 }
