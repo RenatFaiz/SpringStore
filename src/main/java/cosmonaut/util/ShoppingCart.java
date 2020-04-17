86b0388350d41cca97a34c773fe214f15d97197d
RenatFaiz <renatf87@gmail.com>
2020-04-17 16:49:39 +0400

    SCOPE_SESSION, TARGET_CLASS options added to Scope in Shopping cart class

--- src/main/java/cosmonaut/util/ShoppingCart.java
+++ src/main/java/cosmonaut/util/ShoppingCart.java
@@ -4,13 +4,17 @@ import cosmonaut.entity.OrderItem;
 import cosmonaut.entity.Product;
 import cosmonaut.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class ShoppingCart {
 
     private List<OrderItem> orderItems;
